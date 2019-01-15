b36243be532d250267255cef471cb1aa1ab972ff
Kieraj Mumick <kieraj.mumick@airbnb.com>
2019-01-14 19:00:54 -0800

    Revert "Adds Cached State Restoration Before Cell Configuration"
    This reverts commit a4b27fa1b91b2b07174a5880f144c7ef63abd1c6.

--- Epoxy/EpoxyViews/TableView/TableView.swift
+++ Epoxy/EpoxyViews/TableView/TableView.swift
@@ -309,7 +309,6 @@ open class TableView: UITableView, TypedEpoxyInterface, InternalEpoxyInterface {
   // MARK: Private
 
   private var dataIDsForHidingDividers = [String]()
-  private var viewStateCache = [String: RestorableState?]()
 
   private func setUp() {
     delegate = self
@@ -324,20 +323,12 @@ open class TableView: UITableView, TypedEpoxyInterface, InternalEpoxyInterface {
   }
 
   private func configure(cell: Cell, with item: DataType.Item, animated: Bool) {
-    cell.cachedViewState = viewStateCache[item.dataID] ?? nil
-
-    cell.cachedViewStateProvider = { [weak self] state in
-      self?.viewStateCache[item.dataID] = state
-    }
-
     item.configure(cell: cell, forTraitCollection: traitCollection, animated: animated)
     item.setBehavior(cell: cell)
-
     updateDivider(for: cell, dividerType: item.dividerType, dataID: item.dataID)
     if item.isSelectable {
       cell.accessibilityTraits = cell.accessibilityTraits | UIAccessibilityTraitButton
     }
-
   }
 
   private func updateDivider(for cell: TableViewCell, dividerType: EpoxyModelDividerType, dataID: String?) {
