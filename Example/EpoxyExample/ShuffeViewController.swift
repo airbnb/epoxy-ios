9418f703d65fc023e15f844f95141d6374ddeb42
Logan Shire <logan.shire@airbnb.com>
2020-01-24 20:09:14 -0800

    [iOS][Epoxy] Improve example app (#63)
    * Add improved example code
    
    * Make example app test more robust
    
    * Create two tabs, shuffle and standard per comment

---
+++ Example/EpoxyExample/ShuffeViewController.swift
@@ -0,0 +1,64 @@
+// Created by Logan Shire on 1/24/20.
+// Copyright © 2020 Airbnb Inc. All rights reserved.
+
+import Epoxy
+import UIKit
+
+class ShuffleViewController: EpoxyTableViewController {
+
+  // MARK: Properties
+
+  private var timer: Timer?
+
+  // MARK: Initialization
+
+  override init(epoxyLogger: EpoxyLogging = DefaultEpoxyLogger()) {
+    super.init(epoxyLogger: epoxyLogger)
+
+    self.tabBarItem = UITabBarItem.init(tabBarSystemItem: .bookmarks, tag: 0)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: View Lifecycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
+      guard let self = self else {
+        timer.invalidate()
+        return
+      }
+
+      self.updateData(animated: true)
+    }
+  }
+
+  // MARK: EpoxyTableViewController
+
+  override func epoxySections() -> [EpoxySection] {
+    let items = (0..<10)
+      .shuffled()
+      .filter { _ in Int.random(in: 0..<3) % 3 != 0 }
+      .map { dataID -> EpoxyableModel in
+        let text = kTestTexts[dataID]
+        return BaseEpoxyModelBuilder<Row, String>(
+          data: text,
+          dataID: dataID)
+          .configureView { context in
+            context.view.titleText = "Row \(dataID)"
+            context.view.text = text
+          }
+          .didSelect { context in
+            print("DataID selected \(context.dataID) (selection handler)")
+          }
+          .build()
+      }
+
+    return [EpoxySection(items: items)]
+  }
+}
+
---
+++ Example/EpoxyExample/TestData.swift
@@ -0,0 +1,59 @@
+// Created by Logan Shire on 1/23/20.
+// Copyright © 2020 Airbnb Inc. All rights reserved.
+
+import Foundation
+
+let kTestTexts = [
+  kTestText0,
+  kTestText1,
+  kTestText2,
+  kTestText3,
+  kTestText4,
+  kTestText5,
+  kTestText6,
+  kTestText7,
+  kTestText8,
+  kTestText9,
+]
+
+// Test content generated with https://hipsum.co/
+
+let kTestText0 = """
+Lorem ipsum dolor amet four loko authentic kickstarter, seitan shoreditch church-key selvage cornhole. Twee salvia man bun keffiyeh kinfolk. Unicorn brunch kale chips tbh. Tofu taxidermy man braid chicharrones 3 wolf moon farm-to-table, typewriter godard organic gentrify. Farm-to-table try-hard bitters vice. Letterpress bicycle rights retro, fixie marfa fingerstache leggings jianbing. Letterpress semiotics chicharrones, aesthetic cronut cred tumeric.
+"""
+
+let kTestText1 = """
+Tbh edison bulb mumblecore, leggings seitan deep v drinking vinegar retro vexillologist brunch disrupt VHS. Sartorial synth whatever hoodie. Cardigan fanny pack green juice cliche af, biodiesel paleo dreamcatcher hell of ugh farm-to-table.
+"""
+
+let kTestText2 = """
+Cray ugh tbh beard flexitarian, biodiesel DIY freegan palo santo sriracha keytar copper mug. Green juice meggings farm-to-table man braid microdosing forage jean shorts tumblr cardigan cray fixie humblebrag. Polaroid irony crucifix cardigan, chicharrones kale chips vaporware neutra. Fixie af biodiesel chillwave slow-carb food truck. Waistcoat air plant selvage portland polaroid flannel.
+"""
+
+let kTestText3 = """
+Four dollar toast brunch chartreuse, tumblr truffaut before they sold out af letterpress taxidermy chillwave bitters pop-up tilde. Typewriter photo booth man braid aesthetic literally franzen bitters master cleanse food truck chambray readymade. Migas vexillologist aesthetic helvetica enamel pin YOLO, VHS cold-pressed. Deep v lomo tilde gentrify vice. Flannel palo santo iPhone forage hammock schlitz.
+"""
+
+let kTestText4 = """
+Try-hard bushwick iceland freegan scenester meggings shaman. Gentrify hoodie quinoa leggings vice tofu direct trade synth sartorial pickled umami sriracha. Twee chillwave flexitarian, bicycle rights distillery brunch chartreuse retro venmo vegan slow-carb occupy paleo artisan. Schlitz mumblecore kogi mustache. Sartorial try-hard microdosing echo park wayfarers listicle, YOLO affogato williamsburg poutine chillwave hexagon yr. VHS plaid salvia, keytar austin etsy hell of normcore. Shabby chic hoodie viral dreamcatcher, ramps craft beer twee 3 wolf moon.
+"""
+
+let kTestText5 = """
+Farm-to-table drinking vinegar ethical fingerstache, kitsch selvage pok pok letterpress pug unicorn shabby chic. Ugh ramps unicorn, chicharrones offal cardigan chambray kombucha aesthetic readymade polaroid palo santo green juice. Man braid authentic synth post-ironic, poke blue bottle succulents next level godard taxidermy bushwick offal poutine. Everyday carry shaman blog williamsburg. Selvage craft beer mixtape, skateboard squid crucifix brunch. Shaman brooklyn butcher godard ennui irony af kale chips. Try-hard copper mug truffaut dreamcatcher street art actually.
+"""
+
+let kTestText6 = """
+Poke VHS actually tilde fingerstache banh mi air plant paleo. Organic keffiyeh gluten-free enamel pin fam tilde hell of whatever church-key readymade wolf mlkshk franzen. Artisan tofu +1, austin portland occupy authentic edison bulb PBR&B polaroid brooklyn VHS pug chia. Street art iceland authentic, deep v affogato live-edge listicle gentrify la croix succulents.
+"""
+
+let kTestText7 = """
+Keytar schlitz put a bird on it celiac pop-up synth blue bottle. Jianbing hell of fam kale chips put a bird on it scenester gentrify pork belly messenger bag migas enamel pin keffiyeh. Roof party pickled keffiyeh +1 retro vaporware la croix paleo succulents gentrify slow-carb everyday carry poke. Deep v normcore chartreuse, offal aesthetic poke try-hard forage crucifix. Bespoke chambray asymmetrical bitters, cronut letterpress normcore man bun tote bag snackwave scenester. Palo santo skateboard selvage brunch neutra selfies cred chillwave meggings synth polaroid.
+"""
+
+let kTestText8 = """
+Pug hashtag knausgaard master cleanse yuccie squid leggings godard. Yuccie you probably haven't heard of them mumblecore godard street art, listicle +1 jianbing YOLO af chartreuse. Truffaut literally post-ironic distillery. Ennui put a bird on it mustache, gluten-free raclette banjo listicle flannel poutine venmo. Live-edge aesthetic mlkshk la croix. Air plant sriracha tattooed readymade. Umami aesthetic gastropub, art party woke you probably haven't heard of them forage flexitarian shabby chic master cleanse copper mug.
+"""
+
+let kTestText9 = """
+Pug put a bird on it YOLO ramps lo-fi XOXO taiyaki knausgaard. Semiotics activated charcoal tumblr pok pok, chillwave shabby chic portland four loko jianbing skateboard hexagon bushwick banjo swag. Tousled succulents vice stumptown. Microdosing viral master cleanse biodiesel retro, mixtape chillwave.
+"""
