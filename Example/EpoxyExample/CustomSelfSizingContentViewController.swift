4d104d710f74e19a70daeed59ba2ee303d476ba9
Cal Stephens <cal.stephens@airbnb.com>
2020-02-12 13:05:01 -0800

    [iOS] [Epoxy] Improve Split Screen support for TableView and CollectionView
    * Add a new self-sizing example that breaks when using Split Screen
    
    * Recalculate cell heights when TableView.bounds changes
    
    * Use layoutSubviews instead of KVO, and also update CollectionView
    
    * also copy over TableView's traitCollectionDidChange implementation into CollectionView

---
+++ Example/EpoxyExample/CustomSelfSizingContentViewController.swift
@@ -0,0 +1,31 @@
+// Created by Cal Stephens on 2/12/20.
+// Copyright © 2020 Airbnb Inc. All rights reserved.
+
+import UIKit
+import Epoxy
+
+class CustomSelfSizingContentViewController: EpoxyTableViewController {
+
+  override init(epoxyLogger: EpoxyLogging = DefaultEpoxyLogger()) {
+    super.init(epoxyLogger: epoxyLogger)
+    self.tabBarItem = UITabBarItem.init(tabBarSystemItem: .more, tag: 2)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  // MARK: EpoxyTableViewController
+
+  override func epoxySections() -> [EpoxySection] {
+    let items = (0..<10)
+      .map { dataID -> EpoxyableModel in
+        return BaseEpoxyModelBuilder<CustomSizingView, Int>(
+          data: dataID,
+          dataID: dataID)
+          .build()
+      }
+
+    return [EpoxySection(items: items)]
+  }
+}
