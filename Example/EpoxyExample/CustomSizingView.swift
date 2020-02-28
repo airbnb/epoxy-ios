cb1ca4dd2b472ae7d54193c60d6c5e9a9b6b91af
Bryan Keller <bryan.keller@airbnb.com>
2020-02-27 18:31:51 -0800

    Bk/remove extra layout invalidation (#72)
    * Remove extra invalidation from TV and CV
    
    * Make custom sizing view size correctly

--- Example/EpoxyExample/CustomSizingView.swift
+++ Example/EpoxyExample/CustomSizingView.swift
@@ -5,6 +5,8 @@ import UIKit
 
 final class CustomSizingView: UIView {
 
+  // MARK: Lifecycle
+
   init() {
     super.init(frame: .zero)
     setUpViews()
@@ -14,29 +16,66 @@ final class CustomSizingView: UIView {
     fatalError("init(coder:) has not been implemented")
   }
 
-  public override func layoutSubviews() {
-    super.layoutSubviews()
-    if bounds.size != intrinsicContentSize {
-      invalidateIntrinsicContentSize()
+  // MARK: Internal
+
+  override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+    super.traitCollectionDidChange(previousTraitCollection)
+    if traitCollection.preferredContentSizeCategory != .unspecified
+      && previousTraitCollection?.preferredContentSizeCategory != traitCollection.preferredContentSizeCategory
+    {
+      sizingLabel.invalidateIntrinsicContentSize()
     }
   }
 
-  /// Some arbitrary self-sizing that relies on `frame.width`
-  override var intrinsicContentSize: CGSize {
-    let width = frame.width
-    return CGSize(width: width, height: width * 0.3)
-  }
+  // MARK: Private
+
+  // Because this component has an intrinsicContentSize calculation based on its width, it requires
+  // a second layout pass. Only UILabel will receive this second layout pass from the table /
+  // collection view.
+  private let sizingLabel = SizingLabel()
 
   private func setUpViews() {
+    sizingLabel.numberOfLines = 0
+    sizingLabel.contentSize = { width in
+      return CGSize(width: width, height: width * 0.3)
+    }
+    sizingLabel.translatesAutoresizingMaskIntoConstraints = false
+    addSubview(sizingLabel)
+
     let titleLabel = UILabel()
     titleLabel.text = "Self-sizing View"
     titleLabel.translatesAutoresizingMaskIntoConstraints = false
     addSubview(titleLabel)
 
     NSLayoutConstraint.activate([
+      sizingLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
+      sizingLabel.trailingAnchor.constraint(equalTo: trailingAnchor),
+      sizingLabel.topAnchor.constraint(equalTo: topAnchor),
+      sizingLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
       titleLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
       titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
     ])
   }
 
+}
+
+// MARK: - SizingLabel
+
+private class SizingLabel: UILabel {
+
+  var contentSize: ((CGFloat) -> CGSize)?
+  override var intrinsicContentSize: CGSize {
+    let constrainingWidth: CGFloat
+    if preferredMaxLayoutWidth != 0 {
+      constrainingWidth = preferredMaxLayoutWidth
+    } else {
+      constrainingWidth = super.intrinsicContentSize.width
+    }
+
+    return contentSize?(constrainingWidth) ?? .zero
+  }
+
+  override func draw(_ rect: CGRect) {
+    // Do nothing here, the label is only used for sizing
+  }
 }
