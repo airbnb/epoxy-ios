c3a9425c744ff866dd2aa2e70ed65f42e32654fb
Eric Horacek <eric.horacek@airbnb.com>
2021-03-15 17:21:09 -0700

    Add uniform methods to CollectionViewController that take items

--- Sources/EpoxyCollectionView/ViewControllers/CollectionViewController.swift
+++ Sources/EpoxyCollectionView/ViewControllers/CollectionViewController.swift
@@ -21,6 +21,14 @@ open class CollectionViewController: UIViewController {
   }
 
   /// Initializes a collection view controller and configures its collection view with the provided
+  /// layout and a single section containing the given items once the view loads.
+  ///
+  /// The `SectionModel` containing the items has a data ID of `DefaultDataID.noneProvided`.
+  public convenience init(layout: UICollectionViewLayout, items: [ItemModeling]) {
+    self.init(layout: layout, sections: [SectionModel(items: items)])
+  }
+
+  /// Initializes a collection view controller and configures its collection view with the provided
   /// layout and sections once the view loads.
   public convenience init(
     layout: UICollectionViewLayout,
@@ -30,12 +38,14 @@ open class CollectionViewController: UIViewController {
   }
 
   /// Initializes a collection view controller and configures its collection view with the provided
-  /// layout and items once the view loads.
+  /// layout and a single section containing the given items once the view loads.
+  ///
+  /// The `SectionModel` containing the items has a data ID of `DefaultDataID.noneProvided`.
   public convenience init(
     layout: UICollectionViewLayout,
     @ItemModelBuilder items: () -> [ItemModeling])
   {
-    self.init(layout: layout, sections: [SectionModel(items: items())])
+    self.init(layout: layout, items: items())
   }
 
   @available(*, unavailable)
@@ -89,6 +99,17 @@ open class CollectionViewController: UIViewController {
       return
     }
     collectionView.setSections(sections, animated: animated)
+  }
+
+  /// Updates the sections of the `collectionView` to a single section with the provided `items`,
+  /// optionally animating the differences from the current sections.
+  ///
+  /// If `collectionView` has not yet been loaded, the section containing `items` is stored until
+  /// the view loads and set on `collectionView` non-animatedly at that point.
+  ///
+  /// The `SectionModel` containing the items has a data ID of `DefaultDataID.noneProvided`.
+  public func setItems(_ items: [ItemModeling], animated: Bool) {
+    setSections([SectionModel(items: items)], animated: animated)
   }
 
   // MARK: Private
