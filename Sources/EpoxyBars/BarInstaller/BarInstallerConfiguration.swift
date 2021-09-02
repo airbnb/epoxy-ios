c08ec0b62da6374afcc5495249bfc52d5f5b970e
Eric Horacek <eric.horacek@airbnb.com>
2021-09-02 10:35:42 -0600

    Clean up docs

--- Sources/EpoxyBars/BarInstaller/BarInstallerConfiguration.swift
+++ Sources/EpoxyBars/BarInstaller/BarInstallerConfiguration.swift
@@ -25,17 +25,17 @@ public struct BarInstallerConfiguration {
 
   /// A closure that's invoked whenever new bar models are set on a `BarInstaller` following its
   /// initial configuration to customize _when_ those same bars are applied to the underlying
-  /// `BarContainer` by invoking the provided `apply` closure.
+  /// `BarContainer`.
   ///
-  /// For example, when the bar installer is actively participating in a shared element transition,
-  /// this property can be used to wait until the transition is over before apply the bar models to
-  /// the underlying `BarContainer` to ensure that the shared bar elements remain constant over the
-  /// course of the transition.
+  /// For example, if the bar installer is actively participating in a shared element transition,
+  /// this property can be used to defer bar updates until the transition is over to ensure that the
+  /// shared bar elements remain constant over the course of the transition.
   ///
   /// Defaults to `nil`, resulting in any new bars being immediately applied to the underlying
   /// `BarContainer`.
   ///
-  /// Not calling the provided `apply` closure will result in skipped bar model updates.
+  /// Not calling `setBars` on the given `BarContainer` with the provided bars will result in
+  /// skipped bar model updates.
   public var applyBars: ((_ container: BarContainer, _ bars: [BarModeling], _ animated: Bool) -> Void)?
 
 }
