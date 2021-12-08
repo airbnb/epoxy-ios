a6cf7473b52a7437a8e77beebd31d869c6d9a443
Tyler Hedrick <tyler.hedrick@airbnb.com>
2021-12-08 14:06:18 -0800

    [ios] Add example of animated group items inside CollectionView (#63)
    * [ios] Add example of animated group items inside CollectionView
    
    * add xcpretty to builds
    
    * fix function builder errors
    
    * address comments

---
+++ Example/EpoxyExample/ViewControllers/LayoutGroups/DynamicLayoutGroupsViewController.swift
@@ -0,0 +1,68 @@
+// Created by Tyler Hedrick on 11/11/21.
+// Copyright © 2021 Airbnb Inc. All rights reserved.
+
+import Epoxy
+import UIKit
+
+final class DynamicLayoutGroupsViewController: CollectionViewController {
+
+  // MARK: Lifecycle
+
+  init() {
+    super.init(layout: UICollectionViewCompositionalLayout.list)
+    setItems(items, animated: false)
+  }
+
+  // MARK: Private
+
+  private enum DataID: CaseIterable {
+    case row1
+    case row2
+    case row3
+  }
+
+  private var openOptions: [AnyHashable: Bool] = [:]
+
+  private var items: [ItemModeling] {
+    DataID.allCases.map { id in
+      DynamicRow.itemModel(
+        dataID: id,
+        content: .init(
+          title: "Want to know more?",
+          subtitle: "Tap below to reveal a set of options you can choose from",
+          revealOptionsButton: openOptions(id) ? nil : "Reveal options",
+          options: options(for: id),
+          footer: "Thank you"),
+        behaviors: .init(didTapRevealOptions: { [weak self] in
+          self?.openOptions[id] = true
+          self?.updateData()
+        }, didTapOption: { [weak self] option in
+          print("Selected option \(option)")
+          self?.openOptions[id] = false
+          self?.updateData()
+        }))
+    }
+  }
+
+  private func updateData() {
+    setItems(items, animated: true)
+  }
+
+  private func openOptions(_ dataID: AnyHashable) -> Bool {
+    openOptions[dataID] ?? false
+  }
+
+  private func options(for dataID: AnyHashable) -> [String]? {
+    if openOptions(dataID) {
+      return [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+        "Option 5",
+      ]
+    }
+    return nil
+  }
+
+}
