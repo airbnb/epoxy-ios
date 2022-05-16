96b432111f46c6f360b5332f2c568609b12cd196
Lindsay Pond <lindsay.pond@airbnb.com>
2022-05-16 12:02:46 -0700

    [Group]: Update documentation comments

--- Sources/EpoxyLayoutGroups/Groups/VGroup.swift
+++ Sources/EpoxyLayoutGroups/Groups/VGroup.swift
@@ -107,6 +107,13 @@ public final class VGroup: UILayoutGuide, Constrainable, InternalGroup {
   /// Individual alignments on a constrainable take precedence over the group's alignment.
   /// The default value is `.fill`
   public let alignment: ItemAlignment
+
+  /// Layout Animation Parameters are used to determine the animation duration, delay, spring damping ratio, and initial velocity of newly added or removed constrainables
+  /// The default values are:
+  /// duration: `500 ms`,
+  /// delay: `0 ms` delay,
+  /// dampening ratio:`1.0`,
+  /// initial velocity of `0.0`.
   public let springAnimationParameters: SpringAnimationParameters
 
   // MARK: Group
